001de719b5ad85c2784765541b645f396f50966d
TaylorCabassi <taylor.cabassi@my.jcu.edu.au>
2017-08-16 14:03:53 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
 """Taylor"""
+name = input("What is your name?: ")
+for char in name:
+    if char.isdigit():
+        print("Invalid Name")
+        name = input("What is your name?: ")
+    if char.islower() ==0 or char.isupper() == 0:
+        print("Invalid Name")
+        name = input("What is your name?: ")
+print(name[::2])
